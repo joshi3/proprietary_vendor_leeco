50a34bed1e1dfde424ea4802b841dfc18e7f569b
joshi3 <joshi.naga3@gmail.com>
2018-03-06 14:43:21 +0530

    vendor paths shuffle for Oreo

--- x3/x3-vendor-blobs.mk
+++ x3/x3-vendor-blobs.mk
@@ -1,27 +1,5 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/xbin,system/xbin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/vendor,system/vendor)
-
-# Files from app folder
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app/LetvRemoteControl_WW/lib,system/app/LetvRemoteControl_WW/lib)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app/mcRegistry,system/app/mcRegistry)
-
-# App's APKs
-PRODUCT_PACKAGES += \
-	GFManager \
-	LetvRemoteControl_WW \
-	UEIQuicksetSDKLeTV \
-	LocationEM2 \
-	MTKThermalManager \
-
-	PRODUCT_PACKAGES += \
-	FingerSecurity \
-	BatteryChargeLimit
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/vendor,system/vendor)
