30ca30d58d3d89c60f55a5f0d9a69cc57d91a94e
joshi3 <joshi.naga3@gmail.com>
2018-03-10 13:27:06 +0530

    update dependencies for oreo

--- x3/x3-vendor-blobs.mk
+++ x3/x3-vendor-blobs.mk
@@ -1,5 +1,15 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/vendor/bin,$(TARGET_COPY_OUT_VENDOR)/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/vendor/firmware,$(TARGET_COPY_OUT_VENDOR)/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/vendor/xbin,$(TARGET_COPY_OUT_VENDOR)/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/etc,system/etc)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/usr,system/usr)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/xbin,system/xbin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/vendor,system/vendor)
