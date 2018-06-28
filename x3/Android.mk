b7674c6cd41c236667566dae735a26f946da677c
joshi3 <joshi.naga3@gmail.com>
2018-06-28 11:31:20 +0000

    build fix

--- x3/Android.mk
+++ x3/Android.mk
@@ -4,6 +4,17 @@ LOCAL_PATH := $(call my-dir)
 $(info [Decker] copying DP Framework proprietary blobs)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE = libcam.utils.sensorlistener
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = both
+LOCAL_SRC_FILES_32 = proprietary/lib/libcam.utils.sensorlistener.so
+LOCAL_SRC_FILES_64 = proprietary/lib/libcam.utils.sensorlistener.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE = libdpframework
 LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_OWNER = $VENDOR
